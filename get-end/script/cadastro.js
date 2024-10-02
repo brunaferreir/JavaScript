6021f7f61e994e8066f30d8f453d0dbc0200e3a2
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-10-02 20:32:07 -0300

    ok

--- get-end/script/cadastro.js
+++ get-end/script/cadastro.js
@@ -1,20 +1,19 @@
-
 const url = "https://go-wash-api.onrender.com/api/user"; 
 async function cadastroUsuario(){
     let name = document.getElementById('name').value
     let email = document.getElementById('email').value
-    let api = await fetch(url,{
+    let api = await fetch(url,{ /* aguarde a resposta da requisição*/
         method:"POST",
-        body:JSON.stringify({
+        body:JSON.stringify({  /* CONVERTER O OBJETO EM JSON*/
             "name":name,
             "email":email,
             "user_type_id":1,
             "password": "123456",
-            "cpf_cnpj": "00188338020",
+            "cpf_cnpj": "43965119060",
             "terms": 1,
             "birthday":"2000-10-12"    
         }),
-        headers:{
+        headers:{  /* ENVIAR UM JSON E RECEBER UM JSON */
             'Content-Type':'application/json'
         }
     });
@@ -22,8 +21,8 @@ async function cadastroUsuario(){
     if(api.ok){
         let resposta = await api.json();
         console.log(resposta)
-        return
+        return  /* O PRIMEIRO RETURN ENCONTRADA NAO EXECUTA MAIS NADA ABAIXO DA FUNÇÃO */
     }
     let respostaErro = await api.json();
-        console.log(respostaErro.data.errors.cpf_cnpj)
+       alert(respostaErro.data.errors.cpf_cnpj)
 }
