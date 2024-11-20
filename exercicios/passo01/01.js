89ad68666f35584ea1172c6aa33ed680c546798b
bruna ferreira <85415249+brunaferreir@users.noreply.github.com>
2024-11-20 18:00:08 -0300

    ok

--- exercicios/passo01/01.js
+++
@@ -1,2 +0,0 @@
-//1) Escreva um programa que mostre na tela a mensagem "Olá, Mundo!"
-console.log("Oá Mundo!")
---
+++ exercicios/passo01/03.js
@@ -0,0 +1,12 @@
+//3) Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+//Ex:
+//Nome do Funcionário: Maria do Carmo
+//Salário: 1850,45
+//O funcionário Maria do Carmo tem um salário de R$1850,45 em Junho.
+
+const nome = "Ben"
+const salario = 1850.45
+
+console.log(nome)
+console.log(salario)
+console.log("O funcionário", nome,"do Carmo tem um salário de R$",salario ," em Junho.")
